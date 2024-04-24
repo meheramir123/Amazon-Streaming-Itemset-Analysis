c1be1f9e5c3d3f1b8741b8629261700b753220d0
Meher Amir <118044508+meheramir123@users.noreply.github.com>
2024-04-24 20:52:25 +0500

    producer.py

--- producer.py
+++ producer.py
@@ -1,24 +1,14 @@
 from kafka import KafkaProducer
 import json
 
-# Initialize KafkaProducer
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
 
-# Initialize SparkSession
-spark = SparkSession.builder.appName("CSV_Kafka_Producer").getOrCreate()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=json_serializer)
 
-# Load data from CSV
-data = spark.read.text("Ball_by_Ball.csv")
-
-# Convert data to RDD
-rdd = spark.sparkContext.textFile("Ball_by_Ball.csv").map(lambda x: x.split(","))
-
-# Send data to different Kafka topics based on columns
-for doc in rdd.collect():
-    topic = doc[0]  # Assuming the first column is the topic
-    producer.send(topic, doc)
-
-# Close the producer
-producer.close()
-
+with open("dummy_data.json", 'r') as file:
+    for line in file:
+        producer.send("preprocessed_data", value=line)
+        print(line)
+        producer.flush()
